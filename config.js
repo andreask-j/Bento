198ccaf55bf927072d0d903df450df148d3601fb
Andreas Kjær-Jensen <104651675+andreask-j@users.noreply.github.com>
2023-03-04 00:43:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -165,8 +165,8 @@ const CONFIG = {
 					link: 'https://www.linkedin.com',
 				},
 				{
-					name: 'Dribbble',
-					link: 'https://www.dribbble.com',
+					name: 'Tidal',
+					link: 'https://listen.tidal.com/',
 				},
 				{
 					name: 'Trello',
